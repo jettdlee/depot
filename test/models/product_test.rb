839a8ebabb03765c46c42896be0251932831ce5c
Jet-Tsyn Lee <jlee@jlee-Z620-1.yozu.co.uk>
2018-11-22 12:26:08 +0000

    Finished test chapter 7

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
+
   test "produce attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -21,11 +24,49 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal ["> 0.01"], product.errors[:price]
 
-    product.price = -1
+    product.price = 0
     assert product.invalid?
-    assert_equal ["> 0.01"], product.errors[:price]
+    assert_equal ["< 0.01"], product.errors[:price]
     
+    product.price = 1
+    assert product.valid?
+
   end
+
+  def new_product(image_url)
+    Product.new(
+      title: "hsbjhs",
+      description: "kjsbjk",
+      price: 1,
+      image_url: image_url
+    )
+  end
+
+  test "image url" do
+    ok = %w{test.gif test.jpg test.png http://a.b.c/test.gif }
+    bad = %w{test.doc test.gif/more}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(
+      title: products(:ruby).title,
+      description: "yyy",
+      price: 1,
+      image_url: "test.png"
+    )
+
+    assert product.invalid?
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
+
 
 
 
